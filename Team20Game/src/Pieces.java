e090f975dc564389f8d7810d4ee0fd4621a0c2e6
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-02-20 11:13:55 +0100

    Merge branch 'master' of C:\Users\dilaw\Documents\TDAT1005\Systemutvikling Prosjekt\team-20---spillprosjekt with conflicts.

--- Team20Game/src/Pieces.java
+++ Team20Game/src/Pieces.java
@@ -1,15 +1,36 @@
 public abstract class Pieces {
     private boolean color;
+    private int x;
+    private int y;
 
-    public Pieces(boolean color) {
+    public Pieces(boolean color, int x, int y) {
         this.color = color;
+        this.x = this.y;
     }
 
     public boolean getColor() {
         return color;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public void setColor(boolean color) {
         this.color = color;
     }
+
+    public abstract char getNotation();
 }
