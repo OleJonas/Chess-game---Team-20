4870a07e15c624e9dd93507de620fc87967cc6d6
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-02-20 14:13:59 +0100

    fixed major error with a simple trick hehe

--- Team20Game/src/Board.java
+++ Team20Game/src/Board.java
@@ -1,19 +1,20 @@
 public class Board {
-    private Piece[][] position;
+    private Piece[][] position = new Piece[8][8];
 
     public Board() {
         //Default constructor with no arguments will create the starting position of a standard chess game
-        this.position = new Piece[8][8];
+        this.position = position;
 
         //add pawns
         for (int i = 0; i < 8; i++) {
-            this.position[i][1] = new Pawn(true, i, 1);
-            this.position[i][6] = new Pawn(false, i, 6);
+            position[i][1] = new Pawn(true, i, 1);
+            position[i][6] = new Pawn(false, i, 6);
         }
-        this.position[0] = {new Rook(true, 0, 0), new Knight(true, 1, 0), new Bishop(true, 2, 0),  new Queen(true, 3, 0),
-                new King(true, 4, 0), new Bishop(true, 5, 0), new Knight(true, 6, 0),  new Rook(true, 7, 0)};
-        this.position[7] = {new Rook(false, 0, 7), new Knight(false, 1, 7), new Bishop(fals, 2, 7), new Queen(false, 3, 7),
-                new King(false, 4, 7), new Bishop(false, 5, 7), new Knight(false, 6, 7),  new Rook(false, 7, 7)};
+
+        position[0] = new Piece[]{new Rook(true, 0, 0), new Knight(true, 1, 0), new Bishop(true, 2, 0), new Queen(true, 3, 0),
+                new King(true, 4, 0), new Bishop(true, 5, 0), new Knight(true, 6, 0), new Rook(true, 7, 0)};
+        position[7] = new Piece[]{new Rook(false, 0, 7), new Knight(false, 1, 7), new Bishop(false, 2, 7), new Queen(false, 3, 7),
+                new King(false, 4, 7), new Bishop(false, 5, 7), new Knight(false, 6, 7), new Rook(false, 7, 7)};
     }
     public String toString(){
         String a = "";
