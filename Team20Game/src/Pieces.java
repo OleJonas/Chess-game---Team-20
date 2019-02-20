d092c2114f7b02622c1f1c562dbb08b106ab2498
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-02-20 10:44:56 +0100

    added Pieces

--- Team20Game/src/Pieces.java
+++ Team20Game/src/Pieces.java
@@ -1,2 +1,15 @@
 public class Pieces {
+    private boolean color;
+
+    public Pieces(boolean color) {
+        this.color = color;
+    }
+
+    public boolean getColor() {
+        return color;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
 }
