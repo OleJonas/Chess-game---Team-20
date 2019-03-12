a9ae259c0ebd16aa208dfcd38421d7277dc18a6e
Martin Johannes Nilsen <martijni@stud.ntnu.no>
2019-03-12 15:43:20 +0100

    Userprofile

--- Team20Game/src/SQLQueries.sql
+++ Team20Game/src/SQLQueries.sql
@@ -2,8 +2,13 @@
 Create table User(
                    user_id INT AUTO_INCREMENT,
                    username VARCHAR(20),
+                   avatar VARCHAR(15),
+                   gamesPlayed int not null,
+                   gamesWon int not null,
+                   gamesLost int not null,
+                   gamesRemis int not null,
+                   ELOrating int not null,
                    password VARCHAR(250),
                    SALT VARCHAR(250),
-                   avatar VARCHAR(15),
                    PRIMARY KEY (User_id)
 );
