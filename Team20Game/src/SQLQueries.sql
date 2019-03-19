4856a2b4df87a6173bb279c8f8dee07d6ba44aca
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2019-03-19 12:18:44 +0100

    added tables for game, move and chat

--- Team20Game/src/SQLQueries.sql
+++ Team20Game/src/SQLQueries.sql
@@ -12,3 +12,60 @@ Create table User(
                    SALT VARCHAR(250),
                    PRIMARY KEY (User_id)
 );
+
+CREATE TABLE Game(
+game_id INTEGER NOT NULL AUTO_INCREMENT,
+user_id1 INTEGER,
+user_id2 INTEGER,
+result INTEGER,
+time INTEGER NOT NULL,
+increment INTEGER NOT NULL,
+opponent INTEGER,
+CONSTRAINT game_pk PRIMARY KEY (game_id)
+);
+ALTER TABLE Game AUTO_INCREMENT = 100000;
+
+ALTER TABLE Game
+ADD CONSTRAINT game_pk1 FOREIGN KEY(user_id1)
+REFERENCES User(user_id);
+
+ALTER TABLE Game
+ADD CONSTRAINT game_fk2 FOREIGN KEY(user_id2)
+REFERENCES User(user_id);
+
+ALTER TABLE Game
+ADD CONSTRAINT game_fk3 FOREIGN KEY(opponent)
+REFERENCES User(user_id);
+
+
+
+CREATE TABLE Move(
+game_id INTEGER NOT NULL,
+movenr INTEGER NOT NULL,
+fromX INTEGER NOT NULL,
+fromY INTEGER NOT NULL,
+toX INTEGER NOT NULL,
+toY INTEGER NOT NULL,
+CONSTRAINT move_pk PRIMARY KEY(game_id, movenr)
+);
+
+ALTER TABLE Move
+ADD CONSTRAINT move_pk FOREIGN KEY(game_id)
+REFERENCES Game(game_id);
+
+DROP TABLE Chat
+CREATE TABLE Chat(
+msg_id INTEGER AUTO_INCREMENT,
+gameid INTEGER NOT NULL,
+msg VARCHAR(240),
+user_id INTEGER NOT NULL,
+CONSTRAINT chat_pk PRIMARY KEY(msg_id, gameid)
+)
+
+ALTER TABLE Chat
+ADD CONSTRAINT chat_fk FOREIGN KEY(gameid)
+REFERENCES Game(game_id);
+
+ALTER TABLE Chat
+ADD CONSTRAINT chat_fk2 FOREIGN KEY(user_id)
+REFERENCES User(user_id);
