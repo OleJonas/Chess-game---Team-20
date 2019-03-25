53ff0d45390befadcc22ce362ecc92e484b416c5
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2019-03-25 14:39:39 +0100

    made methods in Game

--- Team20Game/src/Database/Game.java
+++ Team20Game/src/Database/Game.java
@@ -1,7 +1,33 @@
 package Database;
 
-public class Game {
-    public static void setResult(int user_id, int game_id){
+import JavaFX.Login;
 
+public class Game {
+    public static void setResult(int game_id, int user_id){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                DBOps db = new DBOps();
+                db.exUpdate("UPDATE Game SET result = " + user_id + "WHERE game_id = "+ game_id);
+            }
+        });
+        t.start();
+    }
+    public static void setUser_id1(int game_id, int user_id){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                DBOps db = new DBOps();
+                db.exUpdate("UPDATE Game SET user_id1 = " + user_id + "WHERE game_id = "+ game_id);
+            }
+        });
+        t.start();
+    }
+    public static void setUser_id2(int game_id, int user_id){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                DBOps db = new DBOps();
+                db.exUpdate("UPDATE Game SET user_id2 = " + user_id + "WHERE game_id = "+ game_id);
+            }
+        });
+        t.start();
     }
 }
