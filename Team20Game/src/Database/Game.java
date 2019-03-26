dc434be70e6e90ec0cb8499c41bb5099f1684757
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2019-03-26 13:52:43 +0100

    fixed promotion in chess demo, both sides can promote

--- Team20Game/src/Database/Game.java
+++ Team20Game/src/Database/Game.java
@@ -5,16 +5,16 @@ import JavaFX.Login;
 public class Game {
     public static int getTime(int game_id){
         DBOps db = new DBOps();
-        return Integer.parseInt(db.exQuery("SELECT time FROM Game WHERE game_id = " + game_id + ", ", 1).get(0));
+        return Integer.parseInt(db.exQuery("SELECT time FROM Game WHERE game_id = " + game_id + "; ", 1).get(0));
     }
     public static int getIncrement(int game_id){
         DBOps db = new DBOps();
-        return Integer.parseInt(db.exQuery("SELECT increment FROM Game WHERE game_id = " + game_id + ",", 1).get(0));
+        return Integer.parseInt(db.exQuery("SELECT increment FROM Game WHERE game_id = " + game_id + ";", 1).get(0));
     }
 
     public static boolean getActive(int game_id){
         DBOps db = new DBOps();
-        if (Integer.parseInt(db.exQuery("SELECT active FROM Game WHERE game_id = " + game_id + ",",1).get(0)) == 1){
+        if (Integer.parseInt(db.exQuery("SELECT active FROM Game WHERE game_id = " + game_id + ";",1).get(0)) == 1){
             return true;
         }
         return false;
@@ -22,8 +22,18 @@ public class Game {
 
     public static int getMode(int game_id){
         DBOps db = new DBOps();
-        return Integer.parseInt(db.exQuery("SELECT mode FROM Game WHERE game_id = " + game_id + ",",1).get(0));
+        return Integer.parseInt(db.exQuery("SELECT mode FROM Game WHERE game_id = " + game_id + ";",1).get(0));
     }
+
+    public static int getUser_id1(int game_id){
+        DBOps db = new DBOps();
+        return Integer.parseInt(db.exQuery("SELECT user_id1 FROM Game WHERE game_id = " + game_id + ";",1).get(0));
+    }
+    public static int getUser_id2(int game_id){
+        DBOps db = new DBOps();
+        return Integer.parseInt(db.exQuery("SELECT user_id2 FROM Game WHERE game_id = " + game_id + ";",1).get(0));
+    }
+
     public static void setResult(int game_id, int user_id){
         Thread t = new Thread(new Runnable() {
             public void run() {
