7def8c5c5d3a5bb4c64d3fef9b0237e4f9f0de9b
tobiaand <tobiaand@stud.ntnu.no>
2019-04-04 13:42:09 +0200

    javaDoc documentation for Piece class

--- Team20Game/src/Pieces/Piece.java
+++ Team20Game/src/Pieces/Piece.java
@@ -64,7 +64,7 @@ public abstract class Piece{
     public abstract char getNotation();
 
     /**
-     * fetch the right image of the piece
+     * fetch image of the piece
      */
     public abstract ImageView getImageView();
 
